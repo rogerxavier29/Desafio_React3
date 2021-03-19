eceb6ca4237cd8f14c5f81ea71232a6b62b86380
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-19 19:06:15 -0300

    App.js
    passado BrowserRouter
    e importado Rotas.js

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Rotas from './Rotas/Rotas';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Rotas />
+    </BrowserRouter>
+  );
 }
 
 export default App;
