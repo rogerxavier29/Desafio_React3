303b493fa4699191f2739c1258adbec269856388
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-23 16:07:56 -0300

    styles.js do Header.js
    importado styled-components.
    
    export const Cabeçalho com header
    
    width: 100% para pegar de ponta a ponta e height: 60px de altura, background roxo de #6959cd,
       p{ padding:20px para o posicionamento fica no meio do header e afastado do canto, passado color: branca, font-weigth: bold negrito e font-size de 17px o tamanho da font

---
+++ portal/src/Header/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Cabeçalho = styled.header`
+  width: 100%;
+  height: 60px;
+  background: #6959cd;
+  p {
+    padding: 20px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 17px;
+  }
+`;
