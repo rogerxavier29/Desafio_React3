6c1909dea296d972782874e92502002b65505d54
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-23 16:02:22 -0300

    Header.js
    Criado pasta e file Header.js
    
    importado Cabeçalho em from styles. como header colocado o um <p> Bem-vindo, filaninho

---
+++ portal/src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Cabeçalho } from './styles';
+
+const Header = () => {
+  return (
+    <Cabeçalho>
+      <p>Bem-vindo, fulaninho</p>
+    </Cabeçalho>
+  );
+};
+
+export default Header;
