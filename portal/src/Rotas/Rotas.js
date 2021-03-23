b42a61e8004ad542527b8bfabf3a50c43709f70d
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-23 16:36:29 -0300

    Rotas.js
    importado HomeAluno,
    
    criado mais uma Route com path"/HomeAluno" e component={HomeAluno}

--- portal/src/Rotas/Rotas.js
+++ portal/src/Rotas/Rotas.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomeAluno from '../HomeAluno/HomeAluno';
 
 import Aluno from '../Login/Aluno';
 import Senha from '../Login/Senha/Senha';
@@ -10,6 +11,7 @@ const Rotas = () => {
       <Switch>
         <Route path="/aluno" component={Aluno} />
         <Route path="/senha" component={Senha} />
+        <Route path="/HomeAluno" component={HomeAluno} />
       </Switch>
     </BrowserRouter>
   );
