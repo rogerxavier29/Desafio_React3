3f542599657dfc24f744a3b9552cfc6053771f2f
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-23 12:01:56 -0300

    styles.js do Senha.js
    DivGeral: acrescentado display:grid, para ficar cada componente um abaixo do outro o form e a foto.
    
    em Titulo = h1: display: grid e justify-content: center para ficar ao centro, color roxa, e padding 40px para colocar o um pouco para abaixo o seu posicionamento.
    
    Form = form: width de 500px e height de 360px para ajeitar seu tamanho, background branco, justify-self:center para o mesmo ficar ao centro e z-index: 10 para o mesmo ficar sobre a imagem.
    
    Imagem = div: justify-self : self-end para o mesmo ficar na ponta final  e margin-top: -180px para o mesmo ficar na mesma tela, sem extrapolar para lado ou para abaixo. E
    img de no max-height de 310px.

--- portal/src/Login/Senha/styles.js
+++ portal/src/Login/Senha/styles.js
@@ -4,4 +4,28 @@ export const DivGeral = styled.div`
   width: 100%;
   height: 100vh;
   background: #dcdcdc;
+  display: grid;
+`;
+
+export const Titulo = styled.h1`
+  display: grid;
+  justify-content: center;
+  color: #6959cd;
+  padding: 40px;
+`;
+
+export const Form = styled.form`
+  width: 500px;
+  height: 360px;
+  background: #fff;
+  justify-self: center;
+  z-index: 10;
+`;
+
+export const Imagem = styled.div`
+  justify-self: self-end;
+  margin-top: -180px;
+  img {
+    max-height: 310px;
+  }
 `;
