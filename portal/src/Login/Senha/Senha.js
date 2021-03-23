dc6ea073e29dce98ff3edf93bb17e4391f60a381
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-23 08:58:15 -0300

    Senha.js
    Criado dentro da pasta Aluno uma outra pasta como file de Senha.js e outro styles.js.
    
    Em Senha.js criado um h1 Esqueci Senha, criado um Form dentro do mesmo uma section e dentro da section dois Li cada um com input type="checkbox"  e cada um com uma label de Estudante e Professor

---
+++ portal/src/Login/Senha/Senha.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Senha = () => {
+  return (
+    <div>
+      <h1>Esqueci Senha</h1>
+      <form>
+        <section>
+          <li>
+            <input type="checkbox" />
+            <label htmlFor="">Estudante</label>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <label htmlFor="">Professor</label>
+          </li>
+        </section>
+      </form>
+    </div>
+  );
+};
+
+export default Senha;
