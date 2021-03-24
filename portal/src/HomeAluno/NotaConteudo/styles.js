3b160facecfe73022971e28e6929b821596bccd3
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-24 18:19:33 -0300

    styles.js do NotaConteud.js
    DivGeral: passado height de 100vh e width 100% para o tamanho pegar toda a tela e background branco.
    
    Linky: passado margin de 40px e em span: text-decoration: none para tirar as cores padrao que ja vem de um link, color: black e font-size: 18px para regular o tamanho da fonte.
    
    Titulo: passado margin: 40px para o distanciamento e font-size de 25px.

---
+++ portal/src/HomeAluno/NotaConteudo/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const DivGeral = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: #fff;
+`;
+
+export const Linky = styled.div`
+  margin: 40px;
+  span {
+    text-decoration: none;
+    color: black;
+    font-size: 18px;
+  }
+`;
+
+export const Titulo = styled.h1`
+  margin: 40px;
+  font-size: 25px;
+`;
