e93bcc455f94fc7806356476746f8e587bf93ea3
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-24 18:13:46 -0300

    NotaConteudo.js
    importado Link react-router-dom.
    
    importado em styles DivGeral como div, Linky como div para poder manipular o Link, Titulo como h1.
    
    Em Link colocado o caminho to="/homealuno" e dentro de link colocado <span>
    
    Dentro de Titulo colocado  Português - Turma 345

--- portal/src/HomeAluno/NotaConteudo/NotaConteudo.js
+++ portal/src/HomeAluno/NotaConteudo/NotaConteudo.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from '../../Header/Header';
+import { Link } from 'react-router-dom';
+
+import { DivGeral, Linky, Titulo } from './styles';
 
 const NotaConteudo = () => {
   return (
-    <div>
+    <DivGeral>
       <Header />
-    </div>
+      <Linky>
+        <Link to="/homealuno">
+          <span>Voltar</span>
+        </Link>
+      </Linky>
+      <Titulo>Português - Turma 345</Titulo>
+    </DivGeral>
   );
 };
 
