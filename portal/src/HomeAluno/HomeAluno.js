ca5c5e31c78657792418e1d9831bdb5a89392f85
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-24 14:11:20 -0300

    HomeAluno.js
    Criado Container como div e dentro Section1, Section2 e Section3 dentro de cada section 4 div cada section.

--- portal/src/HomeAluno/HomeAluno.js
+++ portal/src/HomeAluno/HomeAluno.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import Header from '../Header/Header';
 
-import { DivGeral, Titulo } from './styles';
+import {
+  DivGeral,
+  Titulo,
+  Container,
+  Section1,
+  Section2,
+  Section3,
+} from './styles';
 
 const HomeAluno = () => {
   return (
     <DivGeral>
       <Header />
       <Titulo>Disciplinas:</Titulo>
+      <Container>
+        <Section1>
+          <div>Portugues</div>
+          <div>Portugues</div>
+          <div>Portugues</div>
+          <div>Portugues</div>
+        </Section1>
+        <Section2>
+          <div>Portugues</div>
+          <div>Portugues</div>
+          <div>Portugues</div>
+          <div>Portugues</div>
+        </Section2>
+        <Section3>
+          <div>Portugues</div>
+          <div>Portugues</div>
+          <div>Portugues</div>
+          <div>Portugues</div>
+        </Section3>
+      </Container>
     </DivGeral>
   );
 };
