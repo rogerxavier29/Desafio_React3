a069c720bcabdb204fa52e7da9fa922f03ef6acb
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-24 15:56:29 -0300

    styles.js o Header.js
    em Header passado display: flex, justify-content: space-between para <p> ficar de um lado e <image>
    
    em img{ passado max-height de 40px; referente ao tamanho, pra não ficar muito grande, margin de 15px, pra distanciar do top e das laterais e cursor pointer.

--- portal/src/Header/styles.js
+++ portal/src/Header/styles.js
@@ -4,10 +4,17 @@ export const Cabeçalho = styled.header`
   width: 100%;
   height: 60px;
   background: #6959cd;
+  display: flex;
+  justify-content: space-between;
   p {
     padding: 20px;
     color: #fff;
     font-weight: bold;
     font-size: 17px;
+  }
+  img {
+    max-height: 40px;
+    margin: 15px;
+    cursor: pointer;
   }
 `;
