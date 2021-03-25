932f382addf92cc9f1fdb0e8753cef42f8a2006d
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-25 18:43:44 -0300

    styles.js do ApresentacaoConteudo.js
    em DivGeral{ height de 100vh e width de 100%, e background branco.
    
    em Linky passado margin de 25px para distanciamento e em span{ text-decoration: none, color: black e font-size: 18px.
    
    em Titulo passado margin de 25px, font-size: 25px e cursor pointer .

---
+++ portal/src/Conteudo/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const DivGeral = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: #fff;
+`;
+
+export const Linky = styled.div`
+  margin: 25px;
+  span {
+    text-decoration: none;
+    color: black;
+    font-size: 18px;
+  }
+`;
+
+export const Titulo = styled.h1`
+  margin: 25px;
+  font-size: 25px;
+  cursor: pointer;
+`;
