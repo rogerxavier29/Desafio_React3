f313dff10f9be9aaa317f14b64a977c40d8a1183
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-29 12:19:24 -0300

    Update Aluno.js
    para os Checkboxs. Criado const [checkbox, setCheckBox, useState vazio.
    Criado a função  handleChange com o ({target}) ja desestruturado, e chamando setCheckBox(target.value) que vai puxar o valor Estatico de dentro dos input de checkbox.
    Dentro dos Input de Checkbox, passado type"checkbox", id"Estudante", value="Estudante", em checked={checkebox = "Estudante"} ira verifcar se o checkbox é igual ao nome que foi colocado, deixando selecionar somente um, em onChange={handleChange} chamado a funcao. no input checkbox foi a mesma base, so alterando id e value de Estudante para Professor.
    
    Nos Inputs de Email e senha.
    Definid o estado email e setEmail, senha setSenha, passando o estado " ", vazio.
    Dentro de cada Input de Email e senha.
    o value tem que ser o seu estado definido no caso de Email value={email}, em Senha value={senha} e em onChange foi chamado a função atualizadora do estado de cada um.
    onChange={(event) => setEmail(event.target.value)}, pra senha ... setSenha.
    Tambem em cada um passado id, name, type email, senha type password.
    
    E para form criado função handleSubmit

--- portal/src/Login/Aluno.js
+++ portal/src/Login/Aluno.js
@@ -1,33 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Imagens from '../Imagens/undraw_environmental_study_skau 1.svg';
 
 import { DivGeral, Image, Titulo, DivSenha } from './styles';
 
 const Aluno = () => {
+  const [checkbox, setCheckBox] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(handleSubmit);
+  }
+
+  function handleChange({ target }) {
+    setCheckBox(target.value);
+  }
+
   return (
     <DivGeral>
       <Titulo>Meu Portal</Titulo>
-      <form>
+      <form action="" onSubmit={handleSubmit}>
         <section>
           <li>
-            <input type="checkbox" />
-            <label htmlFor="">Estudante</label>
+            <input
+              type="checkbox"
+              id="Estudante"
+              value="Estudante"
+              checked={checkbox === 'Estudante'}
+              onChange={handleChange}
+            />
+            <label htmlFor="Estudante">Estudante</label>
           </li>
           <li>
-            <input type="checkbox" />
-            <label>Professor</label>
+            <input
+              type="checkbox"
+              id="Professor"
+              value="Professor"
+              checked={checkbox === 'Professor'}
+              onChange={handleChange}
+            />
+            <label htmlFor="Professor">Professor</label>
           </li>
+          {checkbox}
         </section>
         <div>
-          <label>Email</label>
-          <input type="text" />
-          <label>Senha</label>
-          <input type="text" />
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Digite seu Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          {email}
+          <label htmlFor="senha">Senha</label>
+          <input
+            id="senha"
+            type="password"
+            name="senha"
+            placeholder="Digite sua Senha"
+            value={senha}
+            onChange={(event) => setSenha(event.target.value)}
+          />
+          {senha}
           <DivSenha>
             <Link to="/Senha">Esqueci Senha</Link>
           </DivSenha>
-          <button>Entrar</button>
+          <button type="submit">Entrar</button>
         </div>
       </form>
       <Image>
