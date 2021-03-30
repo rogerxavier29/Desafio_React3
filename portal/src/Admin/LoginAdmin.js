4098d2fe761d6f441a13757493fc06f08c559036
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-30 19:32:11 -0300

    Colocado o formulario e  a imagem dentro da DivGeral
    Criado form, dentro de form, uma SectionForm como section com label, input, EsqueSenha como div dentro dela Link de Esqueci Senha, button Entrar. depois criado Image como div e dentro da mesma img.
    
    ----------
    em styles.
    
    em DivGeral passado display: grid.
    
    em Form, passado width 550px, height: 310px; para seu tamanho, passado justify-self: center para ficar ao centro, background branco, margin 20px e z-index 10.
    
    em SectionForm passado display grid, justify-content: center, margin: 30px;
    
    input width de 300px e height de 40px para seu tamanho largura e altura, um border-radius 4px; margin-bottom de 10px.
    em button witdth de 310px e height 40px, border-radius de 4px, margin-top: 20px, color de roxo fraco #6a5acd e background de #fff.
    
    em EsquecSenha, justify-self : self-end;  para o mesmo ficar ao final.
    
    em Image passado justify-self: self-end,
    margin-top: -180px e img max-height de 360px.

--- portal/src/Admin/LoginAdmin.js
+++ portal/src/Admin/LoginAdmin.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import Imagens from '../Imagens/undraw_predictive_analytics_kf9n 1.svg';
+import { Link } from 'react-router-dom';
 
-import { DivGeral, Titulo } from './styles';
+import {
+  DivGeral,
+  Titulo,
+  Form,
+  Image,
+  SectionForm,
+  EsquecSenha,
+} from './styles';
 
 const LoginAdmin = () => {
   return (
     <DivGeral>
       <Titulo>Portal do Administrador</Titulo>
+      <Form action="">
+        <SectionForm>
+          <label htmlFor="">Email</label>
+          <input id="email" type="email" />
+          <label htmlFor="senha">Senha</label>
+          <input id="senha" type="text" />
+          <EsquecSenha>
+            <Link to="">Esqueci Senha</Link>
+          </EsquecSenha>
+          <button type="submit">Entrar</button>
+        </SectionForm>
+      </Form>
+      <Image>
+        <img src={Imagens} />
+      </Image>
     </DivGeral>
   );
 };
--- portal/src/Admin/styles.js
+++ portal/src/Admin/styles.js
@@ -4,9 +4,52 @@ export const DivGeral = styled.div`
   background: #dcdcdc;
   width: 100%;
   height: 100vh;
+  display: grid;
 `;
+
+export const Form = styled.form`
+  width: 550px;
+  height: 310px;
+  justify-self: center;
+  background: #ffff;
+  margin: 20px;
+  z-index: 10;
+`;
+
+export const SectionForm = styled.section`
+  display: grid;
+  justify-content: center;
+  margin: 30px;
+  input {
+    width: 300px;
+    height: 40px;
+    border-radius: 4px;
+    margin-bottom: 10px;
+  }
+  button {
+    width: 310px;
+    height: 40px;
+    border-radius: 4px;
+    margin-top: 20px;
+    color: #6a5acd;
+    background: #fff;
+  }
+`;
+
+export const EsquecSenha = styled.div`
+  justify-self: self-end;
+`;
+
 export const Titulo = styled.h1`
   color: #6a5acd;
   text-align: center;
   padding-top: 40px;
+`;
+
+export const Image = styled.div`
+  justify-self: self-end;
+  margin-top: -180px;
+  img {
+    max-height: 360px;
+  }
 `;
