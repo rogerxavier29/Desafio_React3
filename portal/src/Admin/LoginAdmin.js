56cb53644f53ba569d3d2ebaa8d3107dd85ca7cc
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-03-30 10:22:38 -0300

    Criado pasta Admin e file LoginAdmin.js e styles
    Criado parte inicial do LoginAdmin e estilização
    
    DivGeral como div em styles. Ajustado o tamanho para pegar tudo e passado um cinza fraco.

---
+++ portal/src/Admin/LoginAdmin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { DivGeral } from './styles';
+
+const LoginAdmin = () => {
+  return (
+    <DivGeral>
+      <h1>Portal do Administrador</h1>
+    </DivGeral>
+  );
+};
+
+export default LoginAdmin;
---
+++ portal/src/Admin/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const DivGeral = styled.div`
+  background: #dcdcdc;
+  width: 100%;
+  height: 100vh;
+`;
