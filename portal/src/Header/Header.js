c2630aa1790cd44b93f105437c8afc1172a66923
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-04-06 13:59:10 -0300

    Alterado  e Acrescentado  componentes no Header
    <p> substituido po LiNome que é  uma li em styled-components.
    
    Dentro de Cabeçalho acrescentado uma ul que pega geral, dentro dessa ul tem LiNome que dentro tem um span onde chamamos o nome do usuario com sessionStorage e outra li LiImg onde é chamado a imagem de usuario, dentro dessa li temos uma ul com dois li Meu dados e Sair

--- portal/src/Header/Header.js
+++ portal/src/Header/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Imagens from '../Imagens/Ellipse 1.svg';
 
-import { Cabeçalho } from './styles';
+import { Cabeçalho, LiNome, LiImg } from './styles';
 
 const Header = () => {
   return (
     <Cabeçalho>
-      <p>Bem-vindo, fulaninho</p>
-      <img src={Imagens} />
+      <ul>
+        <LiNome>
+          Bem-vindo, <span>{sessionStorage.getItem('name')}</span>
+        </LiNome>
+        <LiImg>
+          <img src={Imagens} />
+          <ul>
+            <li>Meus Dados</li>
+            <li>
+              <a href="/Loginadmin"> Sair</a>
+            </li>
+          </ul>
+        </LiImg>
+      </ul>
     </Cabeçalho>
   );
 };
