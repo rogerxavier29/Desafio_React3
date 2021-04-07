a56316d13b2e885192f0fde11a21c8de50db5a21
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-04-07 20:35:47 -0300

    Estilização do Form Matricula do Aluno
    em DivVoltar, margin de 40px, font-size de 20px e color:black div do link para voltar de pagina.
    
    Form. passado margin 0 auto para ficar ao centro , margin-top de 40px, para distanciamento ao topo, em input{ passado suas dimensoes.
    
    SectionSelect
    
    margin-top de 50px, largura de 900px e height de 300px, passado display flex para os select ficarem a lados e flex-wrap: wrap, para irem para baixo quando passarem da largura da section e justify-content: space-between para se afastarem os select, em select{ passado suas dimensoes com width e height.
    
    em DivBtn para posicionar o button passado a mesma largura de 900px, display flex e justify-content flex end para o button ir para o lado da direita. em button{
    passado duas dimensoes, border-radius, color e background

---
+++ portal/src/Admin/MatriculaAluno/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const DivVoltar = styled.div`
+  margin: 40px;
+  font-size: 20px;
+  color: black;
+`;
+
+export const Form = styled.form`
+  margin: 0 auto;
+  margin-top: 40px;
+  input {
+    width: 350px;
+    height: 35px;
+  }
+`;
+
+export const SectionSelect = styled.section`
+  margin-top: 50px;
+  width: 900px;
+  height: 300px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  select {
+    width: 360px;
+    height: 40px;
+  }
+`;
+
+export const DivBtn = styled.div`
+  width: 900px;
+  display: flex;
+  justify-content: flex-end;
+  button {
+    width: 200px;
+    height: 35px;
+    border-radius: 8px;
+    background: #fff;
+    color: #0e4da4;
+  }
+`;
