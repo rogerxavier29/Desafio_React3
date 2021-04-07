6b4a7586325a1adf4ca881aa8f1be36820feab1c
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-04-07 20:28:40 -0300

    Pagina MatriculaAluno, feito o formulario
    importado useState,useEffect.
    Importado <Header/> e importado {Link}.
    
    criado valores disciplinas e setDisciplinas deixando seu state vazio.
    
    criado const token, para pegar o token e chama-lo dentro de options em Authorization e criado const url, passando a api.
    
    Chamado useEffect para getdisciplinas e dentro do mesmo criado axios.get e chamado url e options.
    
    No html , dentro de div importado o <Header/>, passado div como DivVoltar e dentro dela chamado o link to="/ homeadmin" para voltar a pagina anterior. Criado form como Form dento do mesmo chamado um input para que sera do n° da matricula do aluno. Depois criado section como SectionSelect criado 6 deles, cada um com duas option a primeira não tera value e sim so presentividade Disciplina a 2° sera para chamar Disciplinas. Depois criado div DivBtn e dentro criado um button para salvar

--- portal/src/Admin/MatriculaAluno/MatriculaAluno.js
+++ portal/src/Admin/MatriculaAluno/MatriculaAluno.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from '../../Header/Header';
+import { Link } from 'react-router-dom';
+
+import { DivVoltar, Form, SectionSelect, DivBtn } from './styles';
+import axios from 'axios';
 
 const MatriculaAluno = () => {
-  return <div></div>;
+  const [disciplinas, setDisciplinas] = useState();
+
+  const token = sessionStorage.getItem('token');
+
+  const options = {
+    headers: {
+      Authorization: `Bearer ${token} `,
+    },
+  };
+
+  const url = `https://projetoportal.herokuapp.com/content/`;
+
+  useEffect(() => {
+    getdisciplinas();
+  }, []);
+  const getdisciplinas = async () => {
+    axios
+      .get(url, options)
+      .then((res) => {
+        console.log(res.data);
+        setDisciplinas(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  return (
+    <div>
+      <Header />
+      <DivVoltar>
+        <Link to="/homeadmin">Voltar</Link>
+        <Form onSubmit="">
+          <input type="text" placeholder="N° de matrícula aluno" />
+          <SectionSelect>
+            <select>
+              <option disabled selected>
+                Disciplinas
+              </option>
+              <option></option>
+            </select>
+            <select>
+              <option disabled selected>
+                Disciplinas
+              </option>
+              <option value=""></option>
+            </select>
+            <select>
+              <option disabled selected>
+                Disciplinas
+              </option>
+              <option value=""></option>
+            </select>
+            <select>
+              <option disabled selected>
+                Disciplinas
+              </option>
+              <option value=""></option>
+            </select>
+            <select>
+              <option disabled selected>
+                Disciplinas
+              </option>
+              <option value=""></option>
+            </select>
+            <select>
+              <option disabled selected>
+                Disciplinas
+              </option>
+              <option value=""></option>
+            </select>
+          </SectionSelect>
+          <DivBtn>
+            <button type="submit">Salvar</button>
+          </DivBtn>
+        </Form>
+      </DivVoltar>
+    </div>
+  );
 };
 
 export default MatriculaAluno;
