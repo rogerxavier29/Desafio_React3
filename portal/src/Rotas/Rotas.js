868b8ccccc0b326df0dda8c49023e2d368756ad9
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-04-25 00:27:37 -0300

    Substituidas Rota aluno e passado alguns params
    Substituido Route /aluno para /loginusuarios
    
    passado params nas routes que foi necessario fazer isso para passar dados para outras paginas.  E acrescentado route /publicarnotas

--- portal/src/Rotas/Rotas.js
+++ portal/src/Rotas/Rotas.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Aluno from '../Login/Aluno';
 import Senha from '../Login/Senha/Senha';
 import HomeAluno from '../HomeAluno/HomeAluno';
 import NotaConteudo from '../HomeAluno/NotaConteudo/NotaConteudo';
@@ -10,20 +9,31 @@ import LoginAdmin from '../Admin/LoginAdmin';
 import HomeAdmin from '../Admin/Home/HomeAdmin';
 import MatriculaAluno from '../Admin/MatriculaAluno/MatriculaAluno';
 import SenhaAdmin from '../Admin/EsqueciSenha/SenhaAdmin';
+import LoginUsuarios from '../Login/LoginUsuarios';
+import VisualizacaoConteudo from '../Conteudo/VisualizacaoConteudo/VisualizacaoConteudo';
+import PublicarNotas from '../Conteudo/Notas/PublicarNotas';
 
 const Rotas = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/aluno" component={Aluno} />
+        <Route path="/loginusuarios" component={LoginUsuarios} />
         <Route path="/senha" component={Senha} />
         <Route path="/homealuno" component={HomeAluno} />
-        <Route path="/notaconteudo" component={NotaConteudo} />
-        <Route path="/apresentconteudo" component={ApresentacaoConteudo} />
+        <Route path="/notaconteudo/:_id/:name" component={NotaConteudo} />
+        <Route
+          path="/apresentconteudo/:_id/:name"
+          component={ApresentacaoConteudo}
+        />
         <Route path="/Loginadmin" component={LoginAdmin} />
         <Route path="/senhaadmin" component={SenhaAdmin} />
         <Route path="/homeadmin" component={HomeAdmin} />
         <Route path="/matriculaluno" component={MatriculaAluno} />
+        <Route
+          path="/visualizarconteudo/:name"
+          component={VisualizacaoConteudo}
+        />
+        <Route path="/publicarnotas/:_id" component={PublicarNotas} />
       </Switch>
     </BrowserRouter>
   );
