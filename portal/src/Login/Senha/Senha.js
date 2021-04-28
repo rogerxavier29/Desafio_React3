fb620e6ad701af04d40a98fa69f8f014e6081b73
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-04-27 23:07:23 -0300

    Passando o valores + api do EsqueciSenha dos usuarios.
    ja tinha feito a estilizado, so não havia passado seus valores etc.
    
    Importado useState, axios.
    passado os estados de  [checkbox, setCheckBox] = useState('');
      const [email, setEmail] = useState('');
      const [cpf, setCpf] = useState('');
      const [novaSenha, setNovaSenha] = useState('');
    e colocados seus values em seus respectivos input.
    
    Criado funcão handleChange para chamar no onChange do Checkbox para alterar seus valor conforme o marcado.
    
    Criado o  const data ={ dentro do mesmo passado email: email, cpf:cpf e password:novaSenha.
    
    Criado a função handleSubmit para chamar a mesma dentro de form em onSubmit.
    Chamado a requisição axios.post passando a api, data.
    
    passado id no Checkbox, value = "3" checked chamando checkbox igual a 3 e em onChange chamando a função handleChange ja falada acima. A mesma coisa para outro Input do type Checkbox o que muda é o valor do value para "3".
    Nos 3 outros inputs colocado seus respectivos values  e em onChange tambem seu value reativo que sera passado e salvo

--- portal/src/Login/Senha/Senha.js
+++ portal/src/Login/Senha/Senha.js
@@ -1,33 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { DivGeral, Titulo, Form, Imagem } from './styles';
 
 import Imagens from '../../Imagens/undraw_environmental_study_skau 1.svg';
+import axios from 'axios';
 
 const Senha = () => {
+  const [checkbox, setCheckBox] = useState('');
+  const [email, setEmail] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [novaSenha, setNovaSenha] = useState('');
+
+  function handleChange({ target }) {
+    setCheckBox(target.value);
+  }
+
+  const data = {
+    email: email,
+    cpf: cpf,
+    password: novaSenha,
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+      .post('https://projetoportal.herokuapp.com/sessions/resetpassword', data)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   return (
     <DivGeral>
       <Titulo>Esqueci Senha</Titulo>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <section>
           <li>
-            <input type="checkbox" />
-            <label htmlFor="">Estudante</label>
+            <input
+              id="Estudante"
+              type="checkbox"
+              value="3"
+              checked={checkbox === '3'}
+              onChange={handleChange}
+            />
+            <label htmlFor="Estudante">Estudante</label>
           </li>
           <li>
-            <input type="checkbox" />
-            <label htmlFor="">Professor</label>
+            <input
+              id="Professor"
+              type="checkbox"
+              value="2"
+              checked={checkbox === '2'}
+              onChange={handleChange}
+            />
+            <label htmlFor="Professor">Professor</label>
           </li>
         </section>
         <div>
           <label htmlFor="">Email</label>
-          <input type="text" placeholder="Email" />
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
 
           <label htmlFor="">CPF</label>
-          <input type="text" placeholder="CPF" />
+          <input
+            type="text"
+            placeholder="CPF"
+            value={cpf}
+            onChange={(event) => setCpf(event.target.value)}
+          />
 
           <label htmlFor="">Nova Senha</label>
-          <input type="text" placeholder="Nova Senha" />
+          <input
+            type="text"
+            placeholder="Nova Senha"
+            value={novaSenha}
+            onChange={(event) => setNovaSenha(event.target.value)}
+          />
 
           <button>Redefinir Senha</button>
         </div>
