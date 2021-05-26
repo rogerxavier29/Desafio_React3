6fc9e3d8f95c47f77d6104d335b7856223c58238
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-05-26 18:25:33 -0300

    Colocado Responsividade
    Colocado responsividade para tablet de 768px
    width e celular 600px de width.
    Em form. acrescentado border-radius 4px.
    
    Nos @media 768px  e nos @media para 600px, foi ajustado principalmente margin-left, margin-top, width e height e font-size.

--- portal/src/Login/styles.js
+++ portal/src/Login/styles.js
@@ -10,6 +10,7 @@ export const DivGeral = styled.div`
   width: 100%;
   height: 100vh;
   padding-top: 0.5px;
+
   form {
     width: 500px;
     height: 380px;
@@ -18,9 +19,23 @@ export const DivGeral = styled.div`
     margin-top: 30px;
     z-index: 10;
     position: absolute;
+    border-radius: 4px;
+
+    @media (max-width: 768px) {
+      width: 350px;
+      margin-left: 150px;
+    }
+
+    @media (max-width: 600px) {
+      margin-left: 70px;
+      width: 320px;
+      height: 340px;
+    }
+
     section {
       display: flex;
       justify-content: center;
+
       li {
         margin: 30px;
       }
@@ -29,14 +44,32 @@ export const DivGeral = styled.div`
       display: grid;
       justify-content: center;
       gap: 10px;
+
       label {
         margin-left: 5px;
         font-family: 'Nunito', sans-serif;
+
+        @media (max-width: 768px) {
+          margin-left: 20px;
+        }
+
+        @media (max-width: 600px) {
+          font-size: 13px;
+          margin-left: 27px;
+        }
       }
       input {
         width: 270px;
         height: 30px;
         border-radius: 4px;
+
+        @media (max-width: 768px) {
+          margin: 0 auto;
+        }
+
+        @media (max-width: 600px) {
+          width: 250px;
+        }
       }
       button {
         margin: 20px 55px;
@@ -46,6 +79,7 @@ export const DivGeral = styled.div`
         background: #fff;
         font-family: 'Nunito', sans-serif;
       }
+
       span {
         font-size: 13px;
         color: red;
@@ -61,6 +95,14 @@ export const DivSenha = styled.div`
   font-size: 14px;
   color: #6959cd;
   font-family: 'Nunito', sans-serif;
+
+  @media (max-width: 768px) {
+    font-size: 13px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 12px;
+  }
 `;
 
 export const Titulo = styled.h1`
@@ -68,14 +110,43 @@ export const Titulo = styled.h1`
   margin-top: 30px;
   color: #6959cd;
   font-family: 'Nunito', sans-serif;
+
+  @media (max-width: 768px) {
+    margin-left: 250px;
+    font-size: 28px;
+  }
+
+  @media (max-width: 600px) {
+    margin-left: 180px;
+    font-size: 23px;
+  }
 `;
 
 export const Image = styled.div`
-  margin-left: 790px;
-  margin-top: 228px;
+  margin-left: 818px;
+  margin-top: 249px;
   position: absolute;
+
+  @media (max-width: 768px) {
+    margin-left: 440px;
+    margin-top: 326px;
+  }
+
+  @media (max-width: 600px) {
+    margin-left: 140px;
+    margin-top: 385px;
+  }
+
   img {
-    max-height: 310px;
+    max-height: 290px;
+
+    @media (max-width: 768px) {
+      max-height: 200px;
+    }
+
+    @media (max-width: 600px) {
+      max-height: 150px;
+    }
   }
 `;
 
