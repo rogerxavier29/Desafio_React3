227f4569d5668d3fb1caa21e8f9097dc47b60de8
rogerxavier29 <62728091+rogerxavier29@users.noreply.github.com>
2021-05-27 18:12:40 -0300

    Ajustado a responsividade
    Ajustado a responsividade para 768px de width para tablet e 600px de width.
    
    Ajustado width, height, font-size, margin-top, max-height e justify-self.

--- portal/src/Admin/EsqueciSenha/styles.js
+++ portal/src/Admin/EsqueciSenha/styles.js
@@ -18,6 +18,14 @@ export const Titulo = styled.h1`
   margin-top: 30px;
   text-align: center;
   font-family: 'Nunito', sans-serif;
+
+  @media (max-width: 768px) {
+    font-size: 23px;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 20px;
+  }
 `;
 
 export const DivForm = styled.div`
@@ -27,33 +35,72 @@ export const DivForm = styled.div`
   margin: 0 auto;
   margin-top: 30px;
   z-index: 100;
+  border-radius: 4px;
+
+  @media (max-width: 768px) {
+    width: 450px;
+    height: 380px;
+  }
+
+  @media (max-width: 600px) {
+    width: 360px;
+    height: 340px;
+  }
+
   form {
     height: 330px;
     display: grid;
     justify-content: center;
     margin-top: 18px;
+
+    @media (max-width: 768px) {
+      height: 300px;
+    }
+
+    @media (max-width: 600px) {
+      height: 270px;
+    }
+
     label {
-      width: 80px;
+      width: 85px;
       height: 0px;
       font-size: 15px;
       font-family: 'Nunito', sans-serif;
+
+      @media (max-width: 768px) {
+        font-size: 14px;
+      }
     }
+
     input {
       width: 300px;
       height: 30px;
       border-radius: 4px;
+
+      @media (max-width: 768px) {
+        width: 280px;
+        height: 28px;
+      }
+
+      @media (max-width: 600px) {
+        width: 260px;
+        height: 26px;
+      }
     }
+
     span {
       font-size: 12px;
       color: red;
       font-family: 'Nunito', sans-serif;
     }
+
     p {
       font-size: 14px;
       color: #2ecc71;
       font-weight: bold;
       font-family: 'Nunito', sans-serif;
     }
+
     button {
       width: 310px;
       height: 35px;
@@ -62,16 +109,35 @@ export const DivForm = styled.div`
       color: #6a5acd;
       background: #fff;
       justify-self: center;
+
+      @media (max-width: 768px) {
+        width: 280px;
+        height: 34px;
+      }
+      @media (max-width: 600px) {
+        width: 260px;
+        height: 33px;
+      }
     }
   }
 `;
 
 export const DivVoltar = styled.div`
   margin: 20px;
+
   p {
     text-decoration: none;
     color: #6a5acd;
     font-family: 'Nunito', sans-serif;
+    font-size: 17px;
+
+    @media (max-width: 768px) {
+      font-size: 15px;
+    }
+
+    @media (max-width: 768px) {
+      font-size: 14px;
+    }
   }
 `;
 
@@ -79,8 +145,25 @@ export const Image = styled.div`
   justify-self: self-end;
   position: relative;
   margin-top: -203px;
+
+  @media (max-width: 768px) {
+    margin-top: -104px;
+  }
+
+  @media (max-width: 600px) {
+    margin-top: -42px;
+    justify-self: center;
+  }
+
   img {
-    max-height: 320px;
-    max-width: 500px;
+    max-height: 295px;
+
+    @media (max-width: 768px) {
+      max-height: 220px;
+    }
+
+    @media (max-width: 600px) {
+      max-height: 200px;
+    }
   }
 `;
